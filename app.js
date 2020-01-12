b8807b31592be21cc6864812cbc39487ee2fc607
daniel <daniel.deneef@gmail.com>
2020-01-12 11:36:52 +0100

    Create the product class into the app.js that will fetch a product from an api

--- app.js
+++ app.js
@@ -10,3 +10,38 @@ const cartContent = document.querySelector('.cart-content');
 const productsDOM = document.querySelector('.products-center');
 //Cart
 let cart = [];
+
+
+
+// getting the produts
+class Products {
+  async getProducts(){
+    try{
+      let result = await fetch('products.json');
+      let data = await result.json();
+
+      let products = data.items;
+
+      products = products.map(item =>{
+        const id = item.id;
+        const title = item.title;
+        const price = item.price;
+        const image = item.image;
+        return {title,price,id,image};
+      })
+      return products;
+      
+    }catch(error){
+      console.log("some error found",error);
+    }    
+  }
+}
+
+//eventlistener
+document.addEventListener("DOMContentLoaded",()=> {
+  const products = new Products();
+
+  //get all products
+  products.getProducts().then(products => console.log(products));
+
+});
